ebfa4c0608a088b310cc8caea0ea6082eb2383cd
hallan-kayo <hallankayo20@gmail.com>
2023-01-30 22:52:07 -0300

    Create Department Class

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Department dep = new Department(1, "eletronics");
+		
+		System.out.println(dep);
+
+	}
+
+}
